3a1850077f4d9da37dce2a0ef2b70397d78f54b9
Tristan <kraig.tri125@gmail.com>
2017-01-15 14:06:22 -0500

    The index file is now hidden on Windows.

--- indexer.py
+++ indexer.py
@@ -7,6 +7,9 @@ import ctypes
 import time
 
 FILE_ATTRIBUTE_HIDDEN = 0x02
+FILE_ATTRIBUTE_NORMAL = 0x80
+WINDOWS_FILE_NOT_FOUND_ERRNO = 2
+
 
 class Indexer:
     def __init__(self, cardList):
@@ -15,16 +18,25 @@ class Indexer:
 
     def createIndex(self):
         filepath = os.path.join('./' + ".card_index")
+        Indexer.make_hidden_file(filepath, False)
+
         with open(filepath, "w", encoding="utf-8-sig", newline="\n") as f:
             json.dump(self.indexMap, f, sort_keys=True, indent=2, separators=(',', ': '))
 
-        # Indexer.make_hidden_file(filepath) #  Unresolved Permission error on overwriting hidden file.
+        Indexer.make_hidden_file(filepath)
 
     # https://stackoverflow.com/questions/25432139/python-cross-platform-hidden-file
     @staticmethod
-    def make_hidden_file(file_name):
+    def make_hidden_file(file_name, hidden=True):
         # For windows set file attribute.
         if os.name == 'nt':
-            ret = ctypes.windll.kernel32.SetFileAttributesW(file_name, FILE_ATTRIBUTE_HIDDEN)
+            fileAttribute = FILE_ATTRIBUTE_HIDDEN
+            if not hidden:
+                fileAttribute = FILE_ATTRIBUTE_NORMAL
+
+            ret = ctypes.windll.kernel32.SetFileAttributesW(file_name, fileAttribute)
             if not ret:  # There was an error
-                raise ctypes.WinError()  # ¯\_(ツ)_/¯
+                if ctypes.WinError().errno == WINDOWS_FILE_NOT_FOUND_ERRNO:
+                    pass
+                else:
+                    raise ctypes.WinError()  # ¯\_(ツ)_/¯
